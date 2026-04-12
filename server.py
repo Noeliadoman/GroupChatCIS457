0d3cb441b4c3755c32cb091ba50d66234a2f4088
Noelia Doman <chavezn@mail.gvsu.edu>
2026-04-12 14:53:22 -0400

    Fix socket binding and decoding issues

--- server.py
+++ server.py
@@ -12,7 +12,7 @@ class Server:
 
     def __init__(self, HOST, PORT):
         self.socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        self.socket.bind({HOST, PORT})
+        self.socket.bind({HOST}, {PORT})
         self.socket.listen(5)
         print('Server waiting for connection...')
 
@@ -21,7 +21,7 @@ def listen(self):
         client_socket, address = self.socket.accept()
         print("Connection from: " + str({address}))
 
-        client_name = client_socket.recv(1024).decode{}
+        client_name = client_socket.recv(1024).decode()
         client = {'client_name': client_name, 'client_socket': client_socket}
         self.broadcast_message(client_name, client_name + " has joined the chat")
         self.Clients.append(client)
@@ -31,7 +31,7 @@ def handle_new_client(self, client):
     client_name = client['client_name']
     client_socket = client['client_socket']
     while True:
-        client_message = client_socket.recv(1024).decode{}
+        client_message = client_socket.recv(1024).decode()
         if client_message.strip() == client_name + ": bye" or not client_message.strip():
             self.broadcast_message(client_name, client_name + " has left the chat")
             Server.Clients.remove(client)
@@ -39,3 +39,13 @@ def handle_new_client(self, client):
             break
         else:
             self.broadcast_message(client_name, client_message)
+def broadcast_message(self, sender_name, message):
+    for client in self.Clients:
+        client_socket = client['client_socket']
+        client_name = client['client_name']
+        if client_name != sender_name:
+            client_socket.send(message.encode())
+
+if __name__ == '__main__':
+    server = Server('127.0.01', 7632)
+    server.listen()
