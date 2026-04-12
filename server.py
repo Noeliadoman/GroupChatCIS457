98a6dc9035054588d574cc036ba9c184758ab43a
Noelia Doman <chavezn@mail.gvsu.edu>
2026-04-12 14:57:42 -0400

    Fix socket binding and address handling in server

--- server.py
+++ server.py
@@ -12,40 +12,41 @@ class Server:
 
     def __init__(self, HOST, PORT):
         self.socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        self.socket.bind({HOST}, {PORT})
+        self.socket.bind((HOST, PORT))
         self.socket.listen(5)
         print('Server waiting for connection...')
 
-def listen(self):
-    while True:
-        client_socket, address = self.socket.accept()
-        print("Connection from: " + str({address}))
+    def listen(self):
+        while True:
+            client_socket, address = self.socket.accept()
+            print("Connection from: " + str({address}))
 
-        client_name = client_socket.recv(1024).decode()
-        client = {'client_name': client_name, 'client_socket': client_socket}
-        self.broadcast_message(client_name, client_name + " has joined the chat")
-        self.Clients.append(client)
-        Thread(target = self.handle_new_client, args = {client}).start()
+            client_name = client_socket.recv(1024).decode()
+            client = {'client_name': client_name, 'client_socket': client_socket}
+            self.broadcast_message(client_name, client_name + " has joined the chat")
+            self.Clients.append(client)
+            Thread(target = self.handle_new_client, args = {client}).start()
 
-def handle_new_client(self, client):
-    client_name = client['client_name']
-    client_socket = client['client_socket']
-    while True:
-        client_message = client_socket.recv(1024).decode()
-        if client_message.strip() == client_name + ": bye" or not client_message.strip():
-            self.broadcast_message(client_name, client_name + " has left the chat")
-            Server.Clients.remove(client)
-            client_socket.close()
-            break
-        else:
-            self.broadcast_message(client_name, client_message)
-def broadcast_message(self, sender_name, message):
-    for client in self.Clients:
-        client_socket = client['client_socket']
+    def handle_new_client(self, client):
         client_name = client['client_name']
-        if client_name != sender_name:
-            client_socket.send(message.encode())
+        client_socket = client['client_socket']
+        while True:
+            client_message = client_socket.recv(1024).decode()
+            if client_message.strip() == client_name + ": bye" or not client_message.strip():
+                self.broadcast_message(client_name, client_name + " has left the chat")
+                Server.Clients.remove(client)
+                client_socket.close()
+                break
+            else:
+                self.broadcast_message(client_name, client_message)
+    def broadcast_message(self, sender_name, message):
+        for client in self.Clients:
+            client_socket = client['client_socket']
+            client_name = client['client_name']
+            if client_name != sender_name:
+                client_socket.send(message.encode())
 
 if __name__ == '__main__':
-    server = Server('127.0.01', 7632)
+    server = Server('127.0.0.1', 7632)
+    server.listen()
     server.listen()
